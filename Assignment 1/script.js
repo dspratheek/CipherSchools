42c495940018e88a62065349bc14d0d7193e10c7
dspratheek <saiprateek.d555@gmail.com>
2022-01-28 18:22:11 +0530

    third commit

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,47 +1,47 @@
 window.onload = function() {
-    var second = 0;
-var minute = 0;
-var appendSeconds = document.getElementById("Seconds");
-var appendMinutes = document.getElementById("Minutes");
-var startbutton = document.getElementById("Start-button");
-var stopbutton = document.getElementById("Stop-button");
-var resetbutton = document.getElementById("Reset-button");
-var Interval;
+var sec = 0;
+var min = 0;
+var appendsecs = document.getElementById("secs");
+var appendmins = document.getElementById("mins");
+var start = document.getElementById("Start-button");
+var stop = document.getElementById("Stop-button");
+var reset = document.getElementById("Reset-button");
+var time;
 
-startbutton.onclick = function() {
-    clearInterval(Interval);
-    Interval = setInterval(startTimer, 1000);
+start.onclick = function() {
+    clearInterval(time);
+    time = setInterval(startTimer, 10);
 }
-stopbutton.onclick = function() {
-    clearInterval(Interval);
+stop.onclick = function() {
+    clearInterval(time);
 }
-resetbutton.onclick = function() {
-    clearInterval(Interval);
-    second = "00";
-    minute = "00";
-    appendSeconds.innerHTML = second;
-    appendMinutes.innerHTML = minute;
+reset.onclick = function() {
+    clearInterval(time);
+    sec = "00";
+    min = "00";
+    appendsecs.innerHTML = sec;
+    appendmins.innerHTML = min;
 }
 
 function startTimer() {
-    second++;
-    if (second <= 9) {
-        appendSeconds.innerHTML = "0" + second;
+    sec++;
+    if (sec <= 9) {
+        appendsecs.innerHTML = "0" + sec;
     }
 
-    if (second > 9 && second < 60) {
-        appendSeconds.innerHTML = second;
+    if (sec > 9 && sec < 60) {
+        appendsecs.innerHTML = sec;
     }
 
-    if (second >= 60) {
-        minute++;
-        appendMinutes.innerHTML = "0" + minute;
-        second = 0;
-        appendSeconds.innerHTML = "0" + second;
+    if (sec >= 60) {
+        min++;
+        appendmins.innerHTML = "0" + min;
+        sec = 0;
+        appendsecs.innerHTML = "0" + sec;
     }
     
-    if (minute > 9){
-        appendMinutes.innerHTML = minute;
+    if (min > 9){
+        appendmins.innerHTML = min;
       }
 }
 }
