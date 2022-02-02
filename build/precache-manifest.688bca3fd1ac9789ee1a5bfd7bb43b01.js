e0fa85c181010c7cee1c36a723222d1cf55ae376
Devarinti Sai Pratheek <68936908+dspratheek@users.noreply.github.com>
2022-02-02 22:27:03 +0530

    Add files via upload

---
+++ build/precache-manifest.688bca3fd1ac9789ee1a5bfd7bb43b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9cb5bf588e44c73a5f9036ad8de8b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fa401a4bd8fc2e13953",
+    "url": "/static/css/main.c8db99a4.chunk.css"
+  },
+  {
+    "revision": "a0513bf2ea8e6e83467c",
+    "url": "/static/js/2.8bc12123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8bc12123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa401a4bd8fc2e13953",
+    "url": "/static/js/main.59c49978.chunk.js"
+  },
+  {
+    "revision": "20dee08e9260ca3a52f4",
+    "url": "/static/js/runtime-main.a0022c02.js"
+  }
+]);
